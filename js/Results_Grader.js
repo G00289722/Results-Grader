d396af0b8980ef2aa40925095d9f0f104451dadc
G00289722 <35769288+G00289722@users.noreply.github.com>
2018-03-15 16:58:33 +0000

    Add files via upload

--- js/Results_Grader.js
+++ js/Results_Grader.js
@@ -1,22 +1,78 @@
 function gradeResult(){
 	
-	var myResult = parseInt(document.getElementById("value1").value);
+	var myResultEnglish = parseInt(document.getElementById("value1").value);
+	var myResultIrish = parseInt(document.getElementById("value2").value);
+	var myResultMaths = parseInt(document.getElementById("value3").value);
 	var grade;
 	
-	if(!myResult){
+	if(!myResultEnglish){
 		alert("Please enter a result");
 	}
 	else {
-		if (myResult >90){
+		if (myResultEnglish >90){
 			grade = 'A';
-		} else if (myResult > 80) {
+		} else if (myResultEnglish > 80) {
 			grade = 'B';
-		} else {
-			grade = 'F';
+		} else if (myResultEnglish > 70) {
+			grade = 'B-';
+		} else if (myResultEnglish > 60) {
+			grade = 'C';
+		} else if (myResultEnglish >50) {
+			grade = 'C-';
+		} else if (myResultEnglish > 40) {
+			grade = 'D';
 		}
 		
-		var message = 'Your result of ' + myResult + ' gets you a grade ' + grade;
+		var message = 'Your result of ' + myResultEnglish + ' gets you a grade ' + grade + ' in English' ;
 		console.log(message);
 		document.getElementById("resultsentence").innerHTML = message; 
+	}
+	
+	
+	if(!myResultIrish){
+		alert("Please enter a result");
+	}
+	else {
+		if (myResultIrish >90){
+			grade = 'A';
+		} else if (myResultIrish > 80) {
+			grade = 'B';
+		} else if (myResultIrish > 70) {
+			grade = 'B-';
+		} else if (myResultIrish > 60) {
+			grade = 'C';
+		} else if (myResultIrish >50) {
+			grade = 'C-';
+		} else if (myResultIrish > 40) {
+			grade = 'D';
+		}
+		
+		var message = 'Your result of ' + myResultIrish + ' gets you a grade ' + grade + ' in Irish' ;
+		console.log(message);
+		document.getElementById("resultsentence2").innerHTML = message; 
+
+	}
+	
+	if(!myResultMaths){
+		alert("Please enter a result");
+	}
+	else {
+		if (myResultMaths >90){
+			grade = 'A';
+		} else if (myResultMaths > 80) {
+			grade = 'B';
+		} else if (myResultMaths > 70) {
+			grade = 'B-';
+		} else if (myResultMaths > 60) {
+			grade = 'C';
+		} else if (myResultMaths>50) {
+			grade = 'C-';
+		} else if (myResultMaths > 40) {
+			grade = 'D';
+		}
+		
+		var message = 'Your result of ' + myResultMaths + ' gets you a grade ' + grade + ' in Maths' ;
+		console.log(message);
+		document.getElementById("resultsentence3").innerHTML = message; 
 	}
 }
